9d4bc369d2922565adcfdc739665d67199882018
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-07-01 11:19:42 +0500

    Added Login modal

--- src/components/VerticalSidebar/VerticalSidebar.js
+++ src/components/VerticalSidebar/VerticalSidebar.js
@@ -1,50 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginModal from "../modals/LoginModal";
 import "./VerticalSidebar.css";
+import { Button } from "react-bootstrap";
 
 const VerticalSidebar = () => {
+  const [loginModal, setLoginModal] = useState(false);
+  const [guideInfoModal, setGuideInfoModal] = useState(false);
+  const [groupModal, setGroupModal] = useState(false);
   return (
-    <div className="vertical-sidebar">
-      <div>
-        <p>Search Guides</p>
-        <hr />
+    <>
+      <div className="vertical-sidebar">
+        <div>
+          <p>Search Guides</p>
+          <hr />
+        </div>
+        <div>
+          <p onClick={() => setLoginModal(true)}>Login</p>
+          <hr />
+        </div>
+        <div>
+          <p>Register</p>
+          <hr />
+        </div>
+        <div>
+          <p>Forget Password</p>
+          <hr />
+        </div>
+        <div>
+          <p>Groups</p>
+          <hr />
+        </div>
+        <div>
+          <p>Guide Info</p>
+          <hr />
+        </div>
+        <div>
+          <p>New Guide</p>
+          <hr />
+        </div>
+        <div>
+          <p>Copy Guide</p>
+          <hr />
+        </div>
+        <div>
+          <p>Chapters</p>
+          <hr />
+        </div>
+        <div>
+          <p>Upload Images</p>
+          <hr />
+        </div>
       </div>
-      <div>
-        <p>Login</p>
-        <hr />
-      </div>
-      <div>
-        <p>Register</p>
-        <hr />
-      </div>
-      <div>
-        <p>Forget Password</p>
-        <hr />
-      </div>
-      <div>
-        <p>Groups</p>
-        <hr />
-      </div>
-      <div>
-        <p>Guide Info</p>
-        <hr />
-      </div>
-      <div>
-        <p>New Guide</p>
-        <hr />
-      </div>
-      <div>
-        <p>Copy Guide</p>
-        <hr />
-      </div>
-      <div>
-        <p>Chapters</p>
-        <hr />
-      </div>
-      <div>
-        <p>Upload Images</p>
-        <hr />
-      </div>
-    </div>
+      <LoginModal show={loginModal} onHide={() => setLoginModal(false)} />
+    </>
   );
 };
 
---
+++ src/components/modals/LoginModal.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import "./Modal.css";
+import img from "../../assets/images/img1.jpg";
+const LoginModal = (props) => {
+  return (
+    <div className="modal">
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        style={{ padding: "50px" }}
+      >
+        <Modal.Body closeButton>
+          <div className="image">
+            <img
+              src={img}
+              alt=""
+              className="text-center rounded-circle"
+              width={130}
+              height={130}
+            />
+          </div>
+          <h4 className="text-center">Login</h4>
+          <Form style={{ width: "60%", margin: "auto" }}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                // style={{ width: "50%", margin: "auto" }}
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" style={{ width: "100%" }}>
+              Login
+            </Button>
+            <p className="text-center">Or</p>
+            <Button variant="primary" type="submit" style={{ width: "100%" }}>
+              Login with Facebook
+            </Button>
+          </Form>
+          <p className="text-center forgot-password">Forgot Password?</p>
+          <p className="register-here">Dont have an account?Register here</p>
+        </Modal.Body>
+        {/* <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer> */}
+      </Modal>
+    </div>
+  );
+};
+
+export default LoginModal;
